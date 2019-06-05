4113d3e90b946e0a8f0f9d78e33d0f6abeda35de
Truc Anh Nguyen Phan <trucphan307@gmail.com>
2019-06-06 00:50:15 +0900

    Update simple OAuth page with button

--- app/src/main/java/com/cs550/teama/spotflickr/activity/auth/FlickrLoginActivity.java
+++ app/src/main/java/com/cs550/teama/spotflickr/activity/auth/FlickrLoginActivity.java
@@ -1,10 +1,13 @@
 package com.cs550.teama.spotflickr.activity.auth;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
@@ -19,16 +22,25 @@ import com.cs550.teama.spotflickr.services.Utils;
 import java.util.Map;
 
 public class FlickrLoginActivity extends AppCompatActivity implements LoginObserver {
-    final static String TAG = "FlickrLoginActivity";
+    private final static String TAG = "FlickrLoginActivity";
     WebView webView;
     ProgressBar progressBar_cyclic;
     SwipeRefreshLayout swipe;
     OAuthService oauth;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_oauth);
+        setContentView(R.layout.activity_flickr_oauth);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginFail();
+            }
+        });
 
         webView = findViewById(R.id.webView);
         progressBar_cyclic = findViewById(R.id.progressBar_cyclic);
@@ -44,7 +56,8 @@ public class FlickrLoginActivity extends AppCompatActivity implements LoginObser
         oauth = new OAuthService(this);
     }
 
-    public void WebAction(String loginUrl){
+    @SuppressLint("SetJavaScriptEnabled")
+    public void WebAction(final String loginUrl){
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
         webView.loadUrl(loginUrl);
@@ -57,21 +70,34 @@ public class FlickrLoginActivity extends AppCompatActivity implements LoginObser
             }
 
             public void onPageFinished(WebView view, String url) {
-                // do your stuff here
                 swipe.setRefreshing(false);
-                // check if there is oauth_verifier in the url.
+            }
+            public void onPageStarted(WebView view, String url, Bitmap favicon){
+                /* Flickr do not have a callback for authentication rejections.
+                   Which means there are no easy way to listen to the "No Thanks" button click.
+                   Our approach is to check if the url goes directly from the loginUrl to flickr.com
+                 */
+                if (webView.canGoBack()){ // If there is a previous URL
+                    String prevUrl = webView.copyBackForwardList().getItemAtIndex(webView.copyBackForwardList().getCurrentIndex()-1).getUrl();
+                    if (prevUrl.equals(loginUrl) && url.equals(OAuthService.FLICKR_URL)){
+                        onLoginFail();
+                    }
+                }
+
+                // Check if there is oauth_verifier in the url.
                 Log.d(TAG, " ----- OAuth step 2 URL: " + url);
                 Map<String, String> queries = Utils.getUrlParameters(url);
                 if (queries != null && queries.containsKey("oauth_verifier")){
                     // The request token is approved
                     // Now get the Access Token with the verifier
-                    Log.d(TAG, "------ APPROVED Request token --------");
+                    System.out.println("------ APPROVED Request token --------");
                     oauth.getAccessToken(queries.get("oauth_verifier"));
                     // hide the webView and show a circle
                     webView.setVisibility(View.GONE);
                     progressBar_cyclic.setVisibility(View.VISIBLE);
                 }
             }
+
         });
     }
 
@@ -95,8 +121,7 @@ public class FlickrLoginActivity extends AppCompatActivity implements LoginObser
         Log.d(TAG, "----- LOGIN FAILED ------");
         Intent resultIntent = new Intent();
         setResult(Activity.RESULT_CANCELED, resultIntent);
-        webView.setVisibility(View.GONE);
-        progressBar_cyclic.setVisibility(View.VISIBLE);
+        finish();
     }
 
     @Override
